ff6a6de97ca22c31a88d1d3bf85dce9df5846f86
oscar <oscar.javier741@gmail.com>
2025-01-03 22:06:25 -0500

    fase 1

---
+++ miniMercado43/src/main/java/com/mycompany/minimercado43/MiniMercado43.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.minimercado43;
+
+/**
+ *
+ * @author OSCAR
+ */
+public class MiniMercado43 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
